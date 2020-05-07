b7b4e7e5d74d0ac6548cd7bd66547f872105e6ab
CHYinYiHang <13393322330@163.com>
2020-05-07 21:49:03 +0800

    1

--- src/test/java/com/sx/springbootexample/SpringBootExampleApplicationTests.java
+++ src/test/java/com/sx/springbootexample/SpringBootExampleApplicationTests.java
@@ -4,6 +4,8 @@ import com.sx.common.utils.JsonUtils;
 import com.sx.common.utils.id.SnowFlake;
 import com.sx.springbootexample.baen.AppConfig;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -29,6 +31,15 @@ class SpringBootExampleApplicationTests {
         }
 
         System.out.println(map);
+    }
+
+    @Test
+    void LoggerTest(){
+        Logger logger = LoggerFactory.getLogger(getClass());
+        logger.info("这是info日志");
+        logger.debug("这是debug日志");
+        logger.warn("这是warn日志");
+        logger.error("这是error日志");
     }
 
 }
